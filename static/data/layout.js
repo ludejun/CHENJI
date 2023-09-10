1f99428af57e083e9feb3a8b47d729c7a3123fe3
ludejun <ludejun07@gmail.com>
2023-09-11 00:22:20 +0800

    辰记自动化发布-6-你好

--- static/data/layout.js
+++ static/data/layout.js
@@ -34,3 +34,9 @@ var layout = {
     layout.logo='';
     layout.githubUrl='https://github.com/ludejun/CHENJI';
     
+
+    /***** 发布时间：2023-09-11 00:22:19（以最后发布的为准）*****/
+    layout.homeUrl='';
+    layout.logo='';
+    layout.githubUrl='https://github.com/ludejun/CHENJI';
+    
